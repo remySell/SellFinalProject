91821a61f65d1aef0fdf10c5181514a46fe5f74d
remySell <r.sell@setonhill.edu>
2020-05-04 21:35:30 -0400

    Initial Commit

---
+++ SellFinalProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SellFinalProject
+//
+//  Created by Remy Sell on 5/4/20.
+//  Copyright © 2020 Remy Sell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
